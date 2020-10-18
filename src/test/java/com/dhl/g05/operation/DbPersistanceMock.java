b1112b13b350b544fab406ff4584f21881ddadf9
Leah Isenor <leah.isenor@dal.ca>
2020-10-17 21:46:35 -0300

    Updated league model to use individual saving and loading methods for each type of object

--- src/test/java/com/dhl/g05/operation/DbPersistanceMock.java
+++ src/test/java/com/dhl/g05/operation/DbPersistanceMock.java
@@ -100,6 +100,9 @@ public class DbPersistanceMock implements IDataBasePersistence{
 	@Override
 	public int loadLeagueObject(String leagueName,LeagueObject leagueObject) {
 		if(leagueName.equalsIgnoreCase("HockeyLeague")) {
+			ArrayList<ConferenceObject> conferences = new ArrayList<>();
+			conferences.add(new ConferenceObject("Western Conference",null));
+			leagueObject.setConferenceDetails(conferences);;
 			return 1;
 		}
 		return 0;
@@ -108,6 +111,9 @@ public class DbPersistanceMock implements IDataBasePersistence{
 	@Override
 	public int loadConferenceObject(int leagueId, ConferenceObject conferenceObject) {
 		if(leagueId==1 && conferenceObject.getConferenceName().equals("Western Conference")) {
+			ArrayList<DivisionObject> divisions = new ArrayList<>();
+			divisions.add(new DivisionObject("Atlantic",null));
+			conferenceObject.setDivisionDetails(divisions);
 			return 1;
 		}
 		return 0;
@@ -116,6 +122,9 @@ public class DbPersistanceMock implements IDataBasePersistence{
 	@Override
 	public int loadDivisionObject(int conferenceId, DivisionObject divisionObject) {
 		if(conferenceId==1 && divisionObject.getDivisionName().equals("Atlantic")) {
+			ArrayList<TeamObject> teams = new ArrayList<>();
+			teams.add(new TeamObject("Striker Six",null,null,null));
+			divisionObject.setTeamDetails(teams);
 			return 1;
 		}
 		return 0;
@@ -124,6 +133,9 @@ public class DbPersistanceMock implements IDataBasePersistence{
 	@Override
 	public int loadTeamObject(int divisionId, TeamObject teamObject) {
 		if(divisionId==1 && teamObject.getTeamName().equals("Striker Six")) {
+			ArrayList<PlayerObject> players = new ArrayList<>();
+			players.add(new PlayerObject("Cristiano Ronaldo",null,null));
+			teamObject.setPlayerList(players);
 			return 1;
 		}
 		return 0;
---
+++ src/test/java/com/dhl/g05/statemachine/LeagueModelTest.java
@@ -0,0 +1,113 @@
+package com.dhl.g05.statemachine;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.dhl.g05.leaguemodel.ConferenceObject;
+import com.dhl.g05.leaguemodel.DivisionObject;
+import com.dhl.g05.leaguemodel.FreeAgentObject;
+import com.dhl.g05.leaguemodel.LeagueObject;
+import com.dhl.g05.leaguemodel.PlayerObject;
+import com.dhl.g05.leaguemodel.TeamObject;
+import com.dhl.g05.operation.DbPersistanceMock;
+
+public class LeagueModelTest {
+	LeagueModel leagueModel;
+	LeagueObject league;
+	TeamObject team;
+	TeamObject team2;
+
+	@Before
+	public void init() {
+		leagueModel = new LeagueModel(new DbPersistanceMock());
+		
+		PlayerObject player = new PlayerObject("Cristiano Ronaldo",null,null);
+		ArrayList<PlayerObject> players = new ArrayList<>();
+		players.add(player);
+		
+		team = new TeamObject("Striker Six",null,null,players);
+		team2 = new TeamObject("t",null,null,players);
+		ArrayList<TeamObject> teams = new ArrayList<>();
+		teams.add(team);
+		
+		DivisionObject division = new DivisionObject("Atlantic",teams);
+		ArrayList<DivisionObject> divisions = new ArrayList<>();
+		divisions.add(division);
+		
+		ConferenceObject conference = new ConferenceObject("Western Conference",divisions);
+		ArrayList<ConferenceObject> conferences = new ArrayList<>();
+		conferences.add(conference);
+
+		league = new LeagueObject("HockeyLeague",conferences,null,new DbPersistanceMock());
+		
+	}
+
+	@Test
+	public void testCreateLeague() {
+		LeagueObject newleague = leagueModel.createLeague("testLeague", new ArrayList<ConferenceObject>(), new ArrayList<FreeAgentObject>());   
+		assertNotNull(newleague);
+		assertTrue(newleague.getLeagueName().equals("testLeague"));
+	}
+
+	@Test
+	public void testGetLeague() {
+		leagueModel.setLeague(league);
+		assertNotNull(leagueModel.getLeague());
+		assertEquals(league,leagueModel.getLeague());
+	}
+
+	@Test
+	public void testSetLeague() {
+		leagueModel.setLeague(league);
+		assertNotNull(leagueModel.getLeague());
+		assertEquals(league,leagueModel.getLeague());
+	}
+
+	@Test
+	public void testPersistLeague() {
+		leagueModel.setLeague(league);
+		assertTrue(leagueModel.persistLeague());
+		
+	}
+
+	@Test
+	public void testAddTeamToCurrentLeague() {
+		leagueModel.setLeague(league);
+		leagueModel.addTeamToCurrentLeague("Western Conference", "Atlantic", team2);
+		assertEquals(team2, leagueModel.getTeamFromLeagueObject(league, "Western Conference", "Atlantic", "t"));
+	}
+	
+	@Test
+	public void testAddTeamToCurrentLeagueFails() {
+		TeamObject team = new TeamObject("t",null,null,null);
+		leagueModel.setLeague(league);
+		leagueModel.addTeamToCurrentLeague("a", "b", team);
+		assertNull(leagueModel.getTeamFromLeagueObject(league, "a", "b", "t"));
+	}
+
+	@Test
+	public void testLoadTeam() {
+		assertTrue(leagueModel.loadTeam("HockeyLeague", "Western Conference", "Atlantic", "Striker Six"));
+		assertNotNull(leagueModel.getCurrentTeam());
+		assertNotNull(leagueModel.getLeague());
+	}
+	
+	@Test
+	public void testLoadTeamFails() {
+		leagueModel.loadTeam("HockeyLeague", "", "", "");
+		assertNull(leagueModel.getCurrentTeam());
+	}
+	
+	@Test
+	public void testGetCurrentTeam() {
+		assertNull(leagueModel.getCurrentTeam());
+		leagueModel.setLeague(league);
+		leagueModel.addTeamToCurrentLeague("Western Conference", "Atlantic", team2);
+		assertEquals(team2,leagueModel.getCurrentTeam());
+	}
+
+}
