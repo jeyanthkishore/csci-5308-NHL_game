d1efc7465a4b43d8afe9ce2958703b6489af32e4
Naitik Prajapati <naitik.prajapati@dal.ca>
2020-10-29 17:28:22 -0300

    Fixing Load class with Maven-assembly

--- src/main/java/com/dhl/g05/Driver.java
+++ src/main/java/com/dhl/g05/Driver.java
@@ -6,7 +6,8 @@ import com.dhl.g05.statemachine.StateMachine;
 
 public class Driver {
 	public static void main(String[] args) {
-		StateMachine stateMachine = new StateMachine(new PlayerCommunication(), new LeagueModelJson());
-		stateMachine.enterState();
+//		StateMachine stateMachine = new StateMachine(new PlayerCommunication(), new LeagueModelJson());
+//		stateMachine.enterState();
+		System.out.println("Hello");
 	}
 }
