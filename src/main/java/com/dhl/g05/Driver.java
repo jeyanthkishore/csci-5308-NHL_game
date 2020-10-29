4b10cc9f374f7c38ec742b3afbfb9e019f71391e
Naitik Prajapati <naitik.prajapati@dal.ca>
2020-10-29 17:20:07 -0300

    Added Scope Variable to load class

--- src/main/java/com/dhl/g05/Driver.java
+++ src/main/java/com/dhl/g05/Driver.java
@@ -6,8 +6,7 @@ import com.dhl.g05.statemachine.StateMachine;
 
 public class Driver {
 	public static void main(String[] args) {
-//		StateMachine stateMachine = new StateMachine(new PlayerCommunication(), new LeagueModelJson());
-//		stateMachine.enterState();
-		System.out.println("Hiii");
+		StateMachine stateMachine = new StateMachine(new PlayerCommunication(), new LeagueModelJson());
+		stateMachine.enterState();
 	}
 }
