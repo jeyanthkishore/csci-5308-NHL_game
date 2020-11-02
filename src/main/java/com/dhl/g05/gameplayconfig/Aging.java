aa6e6a9613d70cc7b7b4dd7268c0edb9907dc0cd
Naitik Prajapati <naitik.prajapati@dal.ca>
2020-11-02 16:12:25 -0400

    Added Validation of Aging and Injury

--- src/main/java/com/dhl/g05/gameplayconfig/Aging.java
+++ src/main/java/com/dhl/g05/gameplayconfig/Aging.java
@@ -27,4 +27,28 @@ public class Aging implements IAging{
     public void setMaximumAge(int maximumAge) {
         this.maximumAge = maximumAge;
     }
+
+    public AgingConstant validate() {
+        if (isMaximumAgeNotValid(maximumAge)) {
+            return AgingConstant.MaximumAgeNotValid;
+        }
+        if (isAverageRetirementAgeNotValid(averageRetirementAge)) {
+            return AgingConstant.AverageRetirementAgeNotValid;
+        }
+        return AgingConstant.Success;
+    }
+
+    public boolean isMaximumAgeNotValid(int age) {
+        if(age <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isAverageRetirementAgeNotValid(int age) {
+        if(age <= 0) {
+            return true;
+        }
+        return false;
+    }
 }
