2acf04d906b534cd9f7ab4ecf4e87dc3ee00d955
Jeyanth Kishore Ramasamy <jy253364@dal.ca>
2020-11-27 18:35:07 +0530

    refs #leaguestimulation conflict fix changes

--- src/test/java/com/dhl/g05/team/TeamModelTest.java
+++ src/test/java/com/dhl/g05/team/TeamModelTest.java
@@ -11,9 +11,6 @@ import java.util.List;
 import org.junit.Test;
 
 import com.dhl.g05.mockdata.JsonMockDataDb;
-import com.dhl.g05.player.GenerateNewPlayers;
-import com.dhl.g05.player.IGenerateNewPlayers;
-import com.dhl.g05.player.IPlayer;
 import com.dhl.g05.player.IPlayer;
 
 public class TeamModelTest{
@@ -29,16 +26,6 @@ public class TeamModelTest{
 		assertNull(object.getPlayerList());
 		object.setPlayerList(data.playerList);
 		assertFalse(object.getPlayerList().isEmpty());
-	}
-
-	@Test
-	public void teamObjectParameterConstructorTest() {
-		JsonMockDataDb data = new JsonMockDataDb();
-		TeamModel object = new TeamModel(data.teamName,data.coachDetails,data.generalManagerName,data.playerList);
-		assertSame(data.teamName,object.getTeamName());
-		assertSame(data.coachDetails,object.getCoachDetails());
-		assertSame(data.generalManagerName,object.getGeneralManagerName());
-		assertSame(data.playerList, object.getPlayerList());
 	}
 
 	@Test
