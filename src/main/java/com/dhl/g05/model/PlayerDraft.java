fcf22eac09339b41e1aa06dd0d04a1e827b59abe
LavanyaNili <nl384866@dal.ca>
2020-11-29 22:29:37 -0400

    Error Handling

--- src/main/java/com/dhl/g05/model/PlayerDraft.java
+++ src/main/java/com/dhl/g05/model/PlayerDraft.java
@@ -10,13 +10,12 @@ import org.apache.logging.log4j.Logger;
 import com.dhl.g05.simulation.ILeagueStanding;
 import com.dhl.g05.simulation.IStandingModel;
 
-
 public class PlayerDraft implements IPlayerDraft {
-	
+
 	static final Logger logger = LogManager.getLogger(PlayerDraft.class);
 	private int draftPickTeamSubstraction;
 	private Map<Integer, List<Map<IStandingModel, IStandingModel>>> pickOrderAfterTrading;
-	
+
 	public int getDraftPickTeamSubstraction() {
 		return draftPickTeamSubstraction;
 	}
@@ -35,106 +34,117 @@ public class PlayerDraft implements IPlayerDraft {
 	}
 
 	public void playerDraft1(ILeagueStanding leaguestanding) {
-		List<IStandingModel> standingForAllConference = leaguestanding.getRankingAcrossLeague();
-		IGenerateNewPlayers g = new GenerateNewPlayers();
-		g.setNumberOfTeams(standingForAllConference.size());
-		List<IPlayer> newPlayers = g.generatePlayers();
-		newPlayers.sort(Comparator.comparing(IPlayer::getPlayerStrength));
+		List<IStandingModel> standingForAllConference = new ArrayList<>();
+		try {
+			standingForAllConference = leaguestanding.getRankingAcrossLeague();
+			IGenerateNewPlayers g = new GenerateNewPlayers();
+			g.setNumberOfTeams(standingForAllConference.size());
+			List<IPlayer> newPlayers = g.generatePlayers();
+			newPlayers.sort(Comparator.comparing(IPlayer::getPlayerStrength));
 
-		List<IStandingModel> teamsEligibleForPickFirst = new ArrayList<>();
-		List<IStandingModel> teamsEligibleForPickFirstReverseOrder = new ArrayList<>();
-		List<IStandingModel> teamsEligibleForPickLater = new ArrayList<>();
-		List<IStandingModel> teamsEligibleForPickLaterReverseOrder = new ArrayList<>();
-		int numberOfTeamsEligibleForPickFirst = 0;
+			List<IStandingModel> teamsEligibleForPickFirst = new ArrayList<>();
+			List<IStandingModel> teamsEligibleForPickFirstReverseOrder = new ArrayList<>();
+			List<IStandingModel> teamsEligibleForPickLater = new ArrayList<>();
+			List<IStandingModel> teamsEligibleForPickLaterReverseOrder = new ArrayList<>();
+			int numberOfTeamsEligibleForPickFirst = 0;
 
-		numberOfTeamsEligibleForPickFirst = (standingForAllConference.size() - draftPickTeamSubstraction);
-		teamsEligibleForPickFirstReverseOrder = standingForAllConference.subList(
-				standingForAllConference.size() - numberOfTeamsEligibleForPickFirst, standingForAllConference.size());
-		for (int i = teamsEligibleForPickFirstReverseOrder.size() - 1; i >= 0; i--) {
-			teamsEligibleForPickFirst.add(teamsEligibleForPickFirstReverseOrder.get(i));
+			numberOfTeamsEligibleForPickFirst = (standingForAllConference.size() - draftPickTeamSubstraction);
+			teamsEligibleForPickFirstReverseOrder = standingForAllConference.subList(
+					standingForAllConference.size() - numberOfTeamsEligibleForPickFirst,
+					standingForAllConference.size());
+			for (int i = teamsEligibleForPickFirstReverseOrder.size() - 1; i >= 0; i--) {
+				teamsEligibleForPickFirst.add(teamsEligibleForPickFirstReverseOrder.get(i));
+			}
+			teamsEligibleForPickLaterReverseOrder = standingForAllConference.subList(0, draftPickTeamSubstraction);
+			for (int i = teamsEligibleForPickLaterReverseOrder.size() - 1; i >= 0; i--) {
+				teamsEligibleForPickLater.add(teamsEligibleForPickFirstReverseOrder.get(i));
+			}
+			createPickOrder(teamsEligibleForPickFirst, teamsEligibleForPickLater);
+			Map<Integer, List<IStandingModel>> teamOrder = createPickOrder(teamsEligibleForPickFirst,
+					teamsEligibleForPickLater);
+			pickNewPlayers(teamOrder, newPlayers);
+		} catch (Exception e) {
+			logger.info("No League Standing, cannot perform player Draft");
 		}
-		teamsEligibleForPickLaterReverseOrder = standingForAllConference.subList(0, draftPickTeamSubstraction);
-		for (int i = teamsEligibleForPickLaterReverseOrder.size() - 1; i >= 0; i--) {
-			teamsEligibleForPickLater.add(teamsEligibleForPickFirstReverseOrder.get(i));
-		}
-		createPickOrder(teamsEligibleForPickFirst, teamsEligibleForPickLater);
-
-		Map<Integer, List<IStandingModel>> teamOrder = createPickOrder(teamsEligibleForPickFirst,
-				teamsEligibleForPickLater);
-		pickNewPlayers(teamOrder, newPlayers);
 	}
 
-	public Map<Integer, List<IStandingModel>> createPickOrder(List<IStandingModel> teamsEligibleForPickFirst,
-			List<IStandingModel> teamsEligibleForPickLater) {
+	public Map<Integer, List<IStandingModel>> createPickOrder(List<IStandingModel> teamsEligibleForPickFirst,List<IStandingModel> teamsEligibleForPickLater) {
 		Map<Integer, List<IStandingModel>> pickOrder = new HashMap<>();
 		Map<Integer, List<Map<IStandingModel, IStandingModel>>> pickOrderAfterTrading = getPickOrderAfterTrading();
-		for (int i = 1; i <= 7; i++) {
-			List<IStandingModel> teamsPickOrder = new ArrayList<>();
-			if (pickOrderAfterTrading.get(i) == null) {
-				continue;
-			} else {
-				for (IStandingModel teamToPick : teamsEligibleForPickFirst) {
-					int size1 = teamsPickOrder.size();
-					List<Map<IStandingModel, IStandingModel>> TeamsTrading = pickOrderAfterTrading.get(i);
-					for (Map<IStandingModel, IStandingModel> teamPairTrading : TeamsTrading) {
-						for (Map.Entry<IStandingModel, IStandingModel> teamPair : teamPairTrading.entrySet()) {
-							if ((teamPair.getKey().getTeam().getTeamName().equals(teamToPick.getTeam().getTeamName()))&& (teamPair.getKey().getConference().getConferenceName().equals(teamToPick.getConference().getConferenceName()))&& (teamPair.getKey().getDivision().getDivisionName().equals(teamToPick.getDivision().getDivisionName()))) {
-								teamsPickOrder.add(teamPair.getValue());
-								logger.info( "Round" + i+ " team pick is "+ teamPair.getValue().getTeam().getTeamName());
-								break;
+		try {
+			for (int i = 1; i <= 7; i++) {
+				List<IStandingModel> teamsPickOrder = new ArrayList<>();
+				if (pickOrderAfterTrading.get(i) == null) {
+					continue;
+				} else {
+					for (IStandingModel teamToPick : teamsEligibleForPickFirst) {
+						int size1 = teamsPickOrder.size();
+						List<Map<IStandingModel, IStandingModel>> TeamsTrading = pickOrderAfterTrading.get(i);
+						for (Map<IStandingModel, IStandingModel> teamPairTrading : TeamsTrading) {
+							for (Map.Entry<IStandingModel, IStandingModel> teamPair : teamPairTrading.entrySet()) {
+								if ((teamPair.getKey().getTeam().getTeamName().equals(teamToPick.getTeam().getTeamName()))&& (teamPair.getKey().getConference().getConferenceName().equals(teamToPick.getConference().getConferenceName())) && (teamPair.getKey().getDivision().getDivisionName().equals(teamToPick.getDivision().getDivisionName()))) {
+									teamsPickOrder.add(teamPair.getValue());
+									logger.info("Round" + i + " team pick is " + teamPair.getValue().getTeam().getTeamName());
+									break;
+								}
 							}
 						}
-					}
-					if (teamsPickOrder.size() == size1) {
-						teamsPickOrder.add(teamToPick);
-						logger.info("Round" + i + " team pick is " + teamToPick.getTeam().getTeamName());
-					}
-				}
-				for (IStandingModel teamToPick : teamsEligibleForPickLater) {
-					int size2 = teamsPickOrder.size();
-					List<Map<IStandingModel, IStandingModel>> TeamsTrading = pickOrderAfterTrading.get(i);
-					for (Map<IStandingModel, IStandingModel> teamPairTrading : TeamsTrading) {
-						for (Map.Entry<IStandingModel, IStandingModel> teamPair : teamPairTrading.entrySet()) {
-							if ((teamPair.getKey().getTeam().getTeamName().equals(teamToPick.getTeam().getTeamName()))&& (teamPair.getKey().getConference().getConferenceName().equals(teamToPick.getConference().getConferenceName()))&& (teamPair.getKey().getDivision().getDivisionName().equals(teamToPick.getDivision().getDivisionName()))) {
-								teamsPickOrder.add(teamPair.getValue());
-								logger.info( "Round" + i+ " team pick is "+ teamPair.getValue().getTeam().getTeamName());
-								break;
-							}
+						if (teamsPickOrder.size() == size1) {
+							teamsPickOrder.add(teamToPick);
+							logger.info("Round" + i + " team pick is " + teamToPick.getTeam().getTeamName());
 						}
 					}
-					if (teamsPickOrder.size() == size2) {
-						teamsPickOrder.add(teamToPick);
-						logger.info("Round" + i + " team pick is " + teamToPick.getTeam().getTeamName());
+					for (IStandingModel teamToPick : teamsEligibleForPickLater) {
+						int size2 = teamsPickOrder.size();
+						List<Map<IStandingModel, IStandingModel>> TeamsTrading = pickOrderAfterTrading.get(i);
+						for (Map<IStandingModel, IStandingModel> teamPairTrading : TeamsTrading) {
+							for (Map.Entry<IStandingModel, IStandingModel> teamPair : teamPairTrading.entrySet()) {
+								if ((teamPair.getKey().getTeam().getTeamName().equals(teamToPick.getTeam().getTeamName()))&& (teamPair.getKey().getConference().getConferenceName().equals(teamToPick.getConference().getConferenceName()))&& (teamPair.getKey().getDivision().getDivisionName().equals(teamToPick.getDivision().getDivisionName()))) {
+									teamsPickOrder.add(teamPair.getValue());
+									logger.info("Round" + i + " team pick is " + teamPair.getValue().getTeam().getTeamName());
+									break;
+								}
+							}
+						}
+						if (teamsPickOrder.size() == size2) {
+							teamsPickOrder.add(teamToPick);
+							logger.info("Round" + i + " team pick is " + teamToPick.getTeam().getTeamName());
+						}
 					}
 				}
+				pickOrder.put(i, teamsPickOrder);
 			}
-			pickOrder.put(i, teamsPickOrder);
+		} catch (Exception e) {
+			logger.info("Team size is less than 16 for Player Drafting.");
 		}
 		return pickOrder;
 	}
 
 	public void pickNewPlayers(Map<Integer, List<IStandingModel>> teamOrder, List<IPlayer> newPlayers) {
-		for (int i = 1; i <= 7; i++) {
-			for (IStandingModel team : teamOrder.get(i)) {
-				List<IPlayer> playersInTeam = team.getTeam().getPlayerList();
-				playersInTeam.add((newPlayers.get(0)));
-				logger.info(" Team "+ team.getTeam().getTeamName()+ "picked player " + newPlayers.get(0).getPlayerName());
-				newPlayers.remove(0);
-				team.getTeam().setPlayerList(playersInTeam);
-			}
-		}
-		callRoasterToAdjustPlayers(teamOrder);
-		}
-	
-
-		public void callRoasterToAdjustPlayers(Map<Integer, List<IStandingModel>> teamOrder) {
-			ITeam teamHavingExcessPlayers = new TeamModel();
+		try {
 			for (int i = 1; i <= 7; i++) {
 				for (IStandingModel team : teamOrder.get(i)) {
-					teamHavingExcessPlayers = team.getTeam();
-					logger.info("Removing excess players for team" + team.getTeam());
-					teamHavingExcessPlayers.adjustTeamRoasterAfterDraft(team.getTeam());
+					List<IPlayer> playersInTeam = team.getTeam().getPlayerList();
+					playersInTeam.add((newPlayers.get(0)));
+					logger.info(" Team " + team.getTeam().getTeamName() + " picked player " + newPlayers.get(0).getPlayerName());
+					newPlayers.remove(0);
+					team.getTeam().setPlayerList(playersInTeam);
 				}
+			}
+			callRoasterToAdjustPlayers(teamOrder);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			logger.info("Not enough new players to replenish");
+		}
+	}
+
+	public void callRoasterToAdjustPlayers(Map<Integer, List<IStandingModel>> teamOrder) {
+		ITeam teamHavingExcessPlayers = new TeamModel();
+		for (int i = 1; i <= 7; i++) {
+			for (IStandingModel team : teamOrder.get(i)) {
+				teamHavingExcessPlayers = team.getTeam();
+				logger.info("Adjusting Team Roaster after Drafting" + team.getTeam());
+				teamHavingExcessPlayers.adjustTeamRoasterAfterDraft(team.getTeam());
 			}
 		}
 	}
+}
--- src/test/java/com/dhl/g05/model/PlayerDraftMock.java
+++ src/test/java/com/dhl/g05/model/PlayerDraftMock.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import com.dhl.g05.simulation.IStandingModel;
 import com.dhl.g05.simulation.StandingModel;
@@ -229,55 +228,7 @@ public class PlayerDraftMock {
 		league.setConferenceDetails(conferenceDetails);
 		return league;
 	}
-	public Map<Integer,List<IStandingModel>> mockTradePick(List<IStandingModel> standing) {
-		Map<Integer,List<IStandingModel>> afterTradePickOrder= new HashMap<>() ;
-		List<IStandingModel> standings = standing;
-		List<IStandingModel> Round1= new ArrayList<>();
-		Round1.add(0, standings.get(3));
-		Round1.add(0,standings.get(1));
-
-		List<IStandingModel> Round2= new ArrayList<>();
-		Round2.add(0, standings.get(1));
-		List<IStandingModel> Round3= new ArrayList<>();
-		Round3.add(standings.get(2));
-		Round3.add(standings.get(3));
-		List<IStandingModel> Round4= new ArrayList<>();
-		Round4.add(standings.get(0));
-		Round4.add(standings.get(1));
-		List<IStandingModel> Round5= new ArrayList<>();
-
-		Round5.add(standings.get(1));
-		Round5.add(standings.get(3));
-		List<IStandingModel> Round6= null;
-		List<IStandingModel> Round7= Round1;
-		afterTradePickOrder.put(1, Round1);
-		afterTradePickOrder.put(2, Round2);
-		afterTradePickOrder.put(3, Round3);
-		afterTradePickOrder.put(4, Round4);
-		afterTradePickOrder.put(5, Round5);
-		afterTradePickOrder.put(6, Round6);
-		afterTradePickOrder.put(7, Round7);
-		for (int i = 1; i <=7; i++)
-		{
-			System.out.println();
-			System.out.print("Round" +(i) + " Teams ");
-			if(afterTradePickOrder.get(i) == null)
-			{
-				continue;
-			}
-			else
-			{
-			List<IStandingModel> a = afterTradePickOrder.get(i);
-			for (IStandingModel standingTeam : a)
-			{
-				System.out.print("  " + standingTeam.getTeam().getTeamName());
-			}
-			
-		}
-		}
-		System.out.println();
-		return afterTradePickOrder;
-	}
+	
 	public Map<Integer, List<Map<IStandingModel,IStandingModel>>> mockTradePickLatest(List<IStandingModel> standing) {
 		List<IStandingModel> standings = standing;
 		Map<Integer, List<Map<IStandingModel,IStandingModel>>> finalRoundtradeTeamPick=new HashMap<>();
