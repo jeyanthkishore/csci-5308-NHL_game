3a9ccfee07497054419a575fedda14bb789e50a1
LavanyaNili <nl384866@dal.ca>
2020-11-30 03:52:30 -0400

    refactoring

--- src/test/java/com/dhl/g05/simulation/statemachine/PlayerDraftStateTest.java
+++ src/test/java/com/dhl/g05/simulation/statemachine/PlayerDraftStateTest.java
@@ -17,8 +17,6 @@ import com.dhl.g05.simulation.SimulationAbstractFactory;
 import com.dhl.g05.simulation.SimulationMockAbstractFactory;
 import com.dhl.g05.simulation.leaguesimulation.ILeagueStanding;
 import com.dhl.g05.simulation.leaguesimulation.IStandingModel;
-import com.dhl.g05.simulation.statemachine.AbstractState;
-import com.dhl.g05.simulation.statemachine.AdvanceToNextSeasonState;
 
 public class PlayerDraftStateTest {
 	private AbstractState state;
@@ -30,7 +28,7 @@ public class PlayerDraftStateTest {
 		state = simulationFactory.createPlayerDraftState();
 		DateHandler.instance().performDateAssignment(2020);
 	}
-	
+
 	@Test
 	public void playerDraftStateTest() {
 		SimulationMockAbstractFactory simulationMockFactory = ApplicationTestConfiguration.instance().getSimulationMockConcreteFactoryState();
@@ -40,13 +38,12 @@ public class PlayerDraftStateTest {
 		leagueStanding.setStandingList(standingList);
 		league.setLeagueStanding(leagueStanding);
 		league.setLeagueCurrentDate(LocalDate.of(2021, Month.JULY, 15));
-		
+
 		state.setLeague(league);
 		state.enter();
 		state.performStateTask();
 		state.exit();
 		assertTrue(state.getNextState() instanceof AdvanceToNextSeasonState);
 	}
-	
-	
+
 }
